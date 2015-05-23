0806715b638b7fa02f616d3d857c3f5cf67c4d99
Jan Ahrens <jan.ahrens+git@googlemail.com>
2015-05-23 20:41:39 +0200

    Integrate with python app

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from routes import route_album, route_newalbum, route_newuser, route_upload
 
 root = Bottle()
 
+@root.route('/')
+def index():
+    return static_file('index.html', root='frontend')
+
 @root.route('/static/<filename:path>')
 def send_static(filename):
     return static_file(filename, root='frontend')
+
+@root.route('/fonts/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='fonts')
 
 root.merge(route_album)
 root.merge(route_newalbum)
