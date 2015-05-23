136c7889234ed5e48bd063d265b3d893c91ea96d
Umorrian <Arne.Steen@gmx.de>
2015-05-23 22:07:08 +0200

    file upload

--- routes/upload.py
+++ routes/upload.py
@@ -1,4 +1,8 @@
-from bottle import Bottle, route
+import os
+
+from bottle import Bottle, route, request
+
+from db import get_db
 
 route_upload = Bottle()
 
@@ -6,19 +10,42 @@ route_upload = Bottle()
 def upload(albumname, userkey):
     # bildupload
     # multiplart upload, all files accepted
+
+    db = get_db()
+
+    cur = db.cursor()
+    cur.execute("SELECT email FROM users WHERE key=?", (userkey, ))
+    user = cur.fetchone()
+    cur.close()
+
+    if user is None:
+        raise 'user does not exist'
+
+    upload = request.files.get('file')
+    name, ext = os.path.splitext(upload.filename)
+    
+    if ext not in ('.png','.jpg','.jpeg'):
+        return {'error' : 'File extension not allowed.'}
+    
+    save_path = "images/"+albumname+'/'
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    upload.save(save_path)
+    
+    cur = db.cursor()
+
+    cur.execute("INSERT INTO images (path, album_url, users_key) VALUES (?, ?, ?)", (save_path+upload.filename, albumname, userkey, ))
+    db.commit()
+
+    cur.close()
+    
     return {
         "images": [
             {
-                "url": "https://myownspace.invalid/photonet/albumname/images/neues+bild.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/neues+bild.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/neues+bild.640.jpg",
-                "uploader": "user1@gmail.com"
-            },
-            {
-                "url": "https://myownspace.invalid/photonet/albumname/images/abschied.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/abschied.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/abschied.640.jpg",
-                "uploader": "user1@gmail.com"
+                "url": "http://localhost:8080/"+save_path+upload.filename,
+                "uploader": user[0],
             }
         ]
     }
