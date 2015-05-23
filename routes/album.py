40030f9ed84279162fed078876f7743182cead06
Jan Ahrens <jan.ahrens+git@googlemail.com>
2015-05-23 21:13:41 +0200

    thumbnails get loaded from server \o/

--- routes/album.py
+++ routes/album.py
@@ -10,39 +10,39 @@ def album(albumname, userkey):
         "images": [
             {
                 "url": "https://myownspace.invalid/photonet/albumname/images/strandfoto.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/strandfoto.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/strandfoto.640.jpg",
+                "thumb320": "/static/example_images/beach-320.jpg",
                 "uploader": "user1@gmail.com"
             },
             {
                 "url": "https://myownspace.invalid/photonet/albumname/images/lagerfeuer.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/lagerfeuer.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/lagerfeuer.640.jpg",
+                "thumb320": "/static/example_images/dog-320.jpg",
                 "uploader": "lemmings@hotmail.fi"
             },
             {
                 "url": "https://myownspace.invalid/photonet/albumname/images/sonnenuntergang+auf+r%C3%BCgen.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/sonnenuntergang+auf+r%C3%BCgen.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/sonnenuntergang+auf+r%C3%BCgen.640.jpg",
+                "thumb320": "/static/example_images/happy-family-320.jpg",
                 "uploader": "lemmings@hotmail.fi"
             },
             {
                 "url": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.640.jpg",
+                "thumb320": "/static/example_images/high-five-320.jpg",
                 "uploader": "lemmings@hotmail.fi"
             },
             {
-                "url": "https://myownspace.invalid/photonet/albumname/images/bootsfahrt.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/bootsfahrt.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/bootsfahrt.640.jpg",
-                "uploader": "user1@gmail.com"
+                "url": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.jpg",
+                "thumb320": "/static/example_images/hiking-family-320.jpg",
+                "uploader": "lemmings@hotmail.fi"
             },
             {
-                "url": "https://myownspace.invalid/photonet/albumname/images/unfall.jpg",
-                "thumb320": "https://myownspace.invalid/photonet/albumname/images/unfall.320.jpg",
-                "thumb640": "https://myownspace.invalid/photonet/albumname/images/unfall.640.jpg",
-                "uploader": "user1@gmail.com"
+                "url": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.jpg",
+                "thumb320": "/static/example_images/cat-320.jpg",
+                "uploader": "lemmings@hotmail.fi"
+            },
+            {
+                "url": "https://myownspace.invalid/photonet/albumname/images/lustige+leute.jpg",
+                "thumb320": "/static/example_images/wedding-320.jpg",
+                "uploader": "lemmings@hotmail.fi"
             }
+
         ]
     }
