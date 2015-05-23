f3762cc4c79c50cf71a66988caeb70a5cdf9d0ce
Umorrian <Arne.Steen@gmx.de>
2015-05-23 15:37:13 +0200

    initial commit

---
+++ controller.py
@@ -0,0 +1,23 @@
+from bottle import route, run
+
+@route('/album/<userkey>', 'POST')
+def newalbum(userkey):
+    return {"key": "value"}
+
+@route('/album/<albumname>/<userkey>/users', 'POST')
+def newuser(albumname, userkey):
+    return {"key": "value"}
+
+@route('/album/<userkey>', 'GET')
+def allalbums(userkey):
+    return {"key": "value"}
+
+@route('/album/<albumname>/<userkey>', 'GET')
+def album(albumname, userkey):
+    return {"key": "value"}
+
+@route('/album/<albumname>/<userkey>/upload', 'POST')
+def upload(albumname, userkey):
+    return {"key": "value"}
+
+run(host='localhost', port=8080)
