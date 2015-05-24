6101754adf1b9fc9604d428a574d024df6eed489
Umorrian <Arne.Steen@gmx.de>
2015-05-24 14:29:06 +0200

    threaded db fix

--- db.py
+++ db.py
@@ -1,8 +1,11 @@
 import sqlite3
 
-def get_db():
-    if not hasattr(get_db, 'db'):
-        db = sqlite3.connect('photonet.db')
-        get_db.db = db
+#def get_db():
+#    if not hasattr(get_db, 'db'):
+#        db = sqlite3.connect('photonet.db')
+#        get_db.db = db
+#
+#    return get_db.db
 
-    return get_db.db
+def get_db():
+    return sqlite3.connect('photonet.db')
