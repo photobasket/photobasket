09e5b63a664481f8ba2a9886e9ca15a406762be7
Umorrian <Arne.Steen@gmx.de>
2015-05-24 14:19:01 +0200

    hackathon awesomeness

--- dbmock.py
+++ dbmock.py
@@ -3,9 +3,9 @@ import sqlite3
 conn = sqlite3.connect('photonet.db')
 cur = conn.cursor()
 
-cur.execute("INSERT INTO album VALUES ('hackathonHH 2015', 'hackathonhh2015', '')")
+cur.execute("INSERT INTO album VALUES ('hackathonHH Awesomeness', 'hackathonhhawesomeness', '')")
 
-cur.execute("INSERT INTO users VALUES ('hackathonhh2015', 'xb5ndH73GHz3jApc', 'hackers@hackathonHH2015.de')")
+cur.execute("INSERT INTO users VALUES ('hackathonhhawesomeness', 'xb5ndH73GHz3jApc', 'hackers@hackathonHH2015.de')")
 
 conn.commit()
 
