1dc7e1985b08eed00f3f4056525fea89ed467ed9
Jan Ahrens <jan.ahrens+git@googlemail.com>
2015-05-24 14:24:40 +0200

    load soundcloud url

--- frontend/application.js
+++ frontend/application.js
@@ -15,6 +15,8 @@ function showAlbum(data) {
   $('h1.album-title').text(data.name);
   $('h1.album-pictures').text(data.images.length + " pictures shared");
   $('.js-download-all').prop('href', data.download_url);
+  $('.js-number-of-friends').text(data.users.length);
+  $('.js-soundcloud').append('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=' + data.soundcloud_url + '&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>');
 
   for (var i = 0; i < data.images.length; i++) {
     $('.container .album').append(
