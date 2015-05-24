d53048c3faf56138944c63731facd082a166b417
Umorrian <Arne.Steen@gmx.de>
2015-05-24 11:27:15 +0200

    live mock data

--- dbmock.py
+++ dbmock.py
@@ -3,10 +3,9 @@ import sqlite3
 conn = sqlite3.connect('photonet.db')
 cur = conn.cursor()
 
-cur.execute("INSERT INTO album VALUES ('Urlaub 2015', 'urlaub2015', 'https://soundcloud.com/philipp-demankowski/philipp-demankowski-balearic-sunday-mix')")
+cur.execute("INSERT INTO album VALUES ('hackathonHH 2015', 'hackathonhh2015', '')")
 
-cur.execute("INSERT INTO users VALUES ('urlaub2015', 'x83hdz7w6x5dbn4t', 'John.Doe@gmail.com')")
-cur.execute("INSERT INTO users VALUES ('urlaub2015', 'lsoe7fh4zf5ndh7e', 'Jane.Doe@gmail.com')")
+cur.execute("INSERT INTO users VALUES ('hackathonhh2015', 'x83hdz7w6x5dbn4t', 'Arne.Steen@gmx.de')")
 
 conn.commit()
 
