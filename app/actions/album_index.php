d616fec7874b5ceed7eea1ae48d92eb5b222db13
Stefan Bauckmeier <emrox@elektronische-musik.de>
2015-05-31 11:17:01 +0200

    example json output on album index

--- app/actions/album_index.php
+++ app/actions/album_index.php
@@ -3,6 +3,36 @@ namespace PhotoBasket;
 
 class AlbumIndexAction extends Action {
 	function main() {
-		echo "Hello World";
+		$data = array(
+			'name'			=> 'Albumname',
+			'soundloud_url'	=> false,
+			'download_url'	=> '/download/dummy.zip',
+			'users'			=> array(
+				'user1@foo.bar',
+				'seconduser@lorem.ipsum'
+			),
+			'images'		=> array(
+				array(
+					'url'		=> 'image1.jpg',
+					'thumb320'	=> 'image1.320.jpg',
+					'size'		=> '800x600',
+					'uploader'	=> 'uploader@example.com'
+				),
+				array(
+					'url'		=> 'image2.jpg',
+					'thumb320'	=> 'image2.320.jpg',
+					'size'		=> '800x600',
+					'uploader'	=> 'uploader@example.com'
+				),
+				array(
+					'url'		=> 'image3.jpg',
+					'thumb320'	=> 'image3.320.jpg',
+					'size'		=> '800x600',
+					'uploader'	=> 'uploader@example.com'
+				)
+			)
+		);
+
+		$this->renderJSON($data);
 	}
 }
--- app/routes/get_rest_album.php
+++ app/routes/get_rest_album.php
@@ -3,7 +3,7 @@
 $app->get('/rest/album/:albumname/:userkey', function ($albumname, $userkey) use ($app) {
 	require_once __DIR__ . '/../actions/album_index.php';
 	new \PhotoBasket\AlbumIndexAction(
-		$app->request,
+		$app,
 		array(
 			'album' => $albumname,
 			'user'	=> $userkey
