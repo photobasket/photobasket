5fc03cc62a38a3a1390672f7558717f55c5e7a2b
Stefan Bauckmeier <emrox@elektronische-musik.de>
2015-06-14 14:43:36 +0200

    added twig template engine

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Slim' => array($vendorDir . '/slim/slim'),
     'Lazer\\Classes\\' => array($vendorDir . '/greg0/lazer-database/src'),
 );
