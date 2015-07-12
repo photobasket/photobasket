a71f8c07498bbc115cc3185100b8b85b3b650152
Stefan Bauckmeier <emrox@elektronische-musik.de>
2015-07-12 16:28:14 +0200

    check album and user on album index

--- app/actions/album_index.php
+++ app/actions/album_index.php
@@ -3,6 +3,15 @@ namespace PhotoBasket;
 
 class AlbumIndexAction extends Action {
 	function main() {
+		if(!$this->check_existence_of_album($this->params['album'])) {
+			return;
+		}
+		$album = DB::get_album($this->params['album']);
+
+		if (!$this->check_existence_of_album_user($this->params['album'], $this->params['user'])) {
+			return;
+		}
+
 		$this->render('album.html');
 	}
 }
